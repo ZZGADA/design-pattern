3c596d81b7033f3bcd62faed26f663c0dc08c3d1
zhangzan <zhangzan@tal.com>
2024-10-14 11:32:09 +0800

    今天讲的是观察者模式，其实理解成主动发送，被动接收会更好理解一点

--- go/behaviourType/observer/main/main.go
+++ go/behaviourType/observer/main/main.go
@@ -1,5 +1,14 @@
 package main
 
+import "observer"
+
 func main() {
+	subject := observer.NewConcreteSubject()
+
+	subject.Register(observer.NewConcreteObserver("1"))
+	subject.Register(observer.NewConcreteObserver("2"))
+	subject.Register(observer.NewConcreteObserver("3"))
+
+	subject.Notify("Hello, Observers!")
 
 }
--- go/behaviourType/observer/observe.go
+++ go/behaviourType/observer/observe.go
@@ -10,6 +10,10 @@ type ConcreteObserver struct {
 	id string
 }
 
+func NewConcreteObserver(id string) *ConcreteObserver {
+	return &ConcreteObserver{id: id}
+}
+
 func (c *ConcreteObserver) Update(data string) {
 	fmt.Printf("Observer %s: Received data: %s\n", c.id, data)
 }
