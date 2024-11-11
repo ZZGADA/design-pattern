d894eba47d023b8bc02bbca733ec4b5ba0e586ca
zhangzan <zhangzan@tal.com>
2024-11-11 22:37:33 +0800

    webRTC

--- go/webrtc/go.mod
+++ go/webrtc/go.mod
@@ -1,3 +1,13 @@
 module webrtc
 
 go 1.23.0
+
+require github.com/pion/stun v0.6.1
+
+require (
+	github.com/pion/dtls/v2 v2.2.7 // indirect
+	github.com/pion/logging v0.2.2 // indirect
+	github.com/pion/transport/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+)
--- go/webrtc/main/main.go
+++ go/webrtc/main/main.go
@@ -1,1 +1,68 @@
 package main
+
+import (
+	"fmt"
+	"github.com/pion/stun"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	// STUN服务器地址
+	stunServerAddr := "8.130.14.29:53478"
+
+	// 创建UDP连接
+	conn, err := net.Dial("udp", stunServerAddr)
+	if err != nil {
+		log.Fatalf("Failed to connect to STUN server: %v", err)
+	}
+	defer conn.Close()
+
+	// 创建STUN客户端
+	client, err := stun.NewClient(conn)
+	if err != nil {
+		log.Fatalf("Failed to create STUN client: %v", err)
+	}
+	defer client.Close()
+
+	// 设置用户名和密码
+	username := "ZZGEDA"
+	password := "zZ-szshyjbz16D"
+	realm := "example.com"
+
+	// 生成HA1
+	ha1 := stun.NewLongTermIntegrity(username, realm, password)
+
+	stunUdp(ha1, client)
+
+	fmt.Println("准备进入轮询")
+
+	for i := 0; i < 100; i++ {
+		time.Sleep(time.Second * 3)
+		stunUdp(ha1, client)
+	}
+
+	fmt.Println("轮询结束")
+}
+
+func stunUdp(ha1 stun.MessageIntegrity, client *stun.Client) {
+	// 发送绑定请求
+	message := stun.MustBuild(stun.TransactionID, stun.BindingRequest, ha1)
+	if err := client.Do(message, func(res stun.Event) {
+		if res.Error != nil {
+			log.Fatalf("STUN request failed: %v", res.Error)
+		}
+
+		// 解析绑定响应
+		var xorAddr stun.XORMappedAddress
+		if err := xorAddr.GetFrom(res.Message); err != nil {
+			log.Fatalf("Failed to get XOR-MAPPED-ADDRESS: %v", err)
+		}
+
+		// 打印公共IP地址和端口号
+		fmt.Printf("Public IP: %s, Port: %d\n", xorAddr.IP, xorAddr.Port)
+	}); err != nil {
+		log.Fatalf("Failed to send STUN request: %v", err)
+	}
+}
